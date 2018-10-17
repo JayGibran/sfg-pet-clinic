6feca9da892667ce8ebf0513ce2dd80b07e9c5e8
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2018-10-17 21:20:27 +0100

    Implement Interfaces for Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/learning/springframework/sfgpetclinic/service/PetService.java
@@ -0,0 +1,15 @@
+package learning.springframework.sfgpetclinic.service;
+
+import learning.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+
+}
