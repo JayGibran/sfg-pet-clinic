994def33bd5a46d7c1eff4b2d9f7cae079a833bb
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2018-10-21 17:38:08 +0100

    Refactored Map Impl to abstract id Generation. closes #23

--- pet-clinic-web/src/main/java/learning/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/learning/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,28 +23,25 @@ public class DataLoader implements CommandLineRunner {
         Owner owner = new Owner();
         owner.setFirstName("Emiriam");
         owner.setLastName("Caroline");
-        owner.setId(1l);
+
 
         ownerService.save(owner);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Jay");
         owner1.setLastName("Amaral");
-        owner1.setId(2l);
 
         ownerService.save(owner1);
 
         System.out.println("Saving owners");
 
         Vet vet = new Vet();
-        vet.setId(1l);
         vet.setFirstName("Logan");
         vet.setLastName("Logan");
 
         vetService.save(vet);
 
         Vet vet1 = new Vet();
-        vet1.setId(2l);
         vet1.setFirstName("Max");
         vet1.setLastName("Max");
 
