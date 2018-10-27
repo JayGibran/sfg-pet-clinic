74ee712125da40586252010caa880968efab8179
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2018-10-27 17:07:24 +0100

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/learning/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package learning.springframework.sfgpetclinic.services;
+
+import learning.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+    
+
+}
