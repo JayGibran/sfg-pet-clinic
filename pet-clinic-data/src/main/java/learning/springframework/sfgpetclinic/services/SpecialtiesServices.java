4c8a978134f457e748187c80f387262f2f3d40f0
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2018-10-27 20:00:37 +0100

    added Specialties Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/learning/springframework/sfgpetclinic/services/SpecialtiesServices.java
@@ -0,0 +1,6 @@
+package learning.springframework.sfgpetclinic.services;
+
+import learning.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesServices extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/learning/springframework/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package learning.springframework.sfgpetclinic.services.map;
+
+import learning.springframework.sfgpetclinic.model.Speciality;
+import learning.springframework.sfgpetclinic.services.SpecialtiesServices;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+    public class SpecialityServiceMap extends AbastractMapService<Speciality, Long> implements SpecialtiesServices {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
