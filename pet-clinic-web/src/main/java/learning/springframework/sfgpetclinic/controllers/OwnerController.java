3fe94d39f892b051add3af4c983430e42eaf63c1
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2018-10-28 13:57:25 +0000

    Fixed Broken Links. Closes #45

--- pet-clinic-web/src/main/java/learning/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/learning/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -20,7 +20,12 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
+    }
 
+    @RequestMapping("/find")
+    public String findOwners(){
+
+        return "notImplemented";
     }
 
 }
