295c5712878da7615f68e7905087b41b96126d43
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2018-11-03 20:23:37 +0000

    added spring data jpa for owner. closes #40

---
+++ pet-clinic-data/src/main/java/learning/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,53 @@
+package learning.springframework.sfgpetclinic.services.springdatajpa;
+
+import learning.springframework.sfgpetclinic.model.Owner;
+import learning.springframework.sfgpetclinic.repositories.OwnerRepository;
+import learning.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    @Autowired
+    private OwnerRepository repository;
+
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return repository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        repository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return repository.findById(aLong).orElse(null);
+
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return repository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        repository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        repository.deleteById(aLong);
+    }
+}
