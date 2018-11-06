cc4edf28afe40202bed389acfa2bbd5f720fc3da
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2018-11-06 17:46:12 +0000

    Implemented Map Impl for Visit Service. closes #46

---
+++ pet-clinic-data/src/main/java/learning/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package learning.springframework.sfgpetclinic.services;
+
+import learning.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
